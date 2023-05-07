c83710c1fbb937f03471ea54cc443fb404a70540
vali <chvaly@gmail.com>
2023-05-07 22:18:18 +0300

    Add project

---
+++ src/main/java/com/vali/springpojo/SpringPojoApplication.java
@@ -0,0 +1,13 @@
+package com.vali.springpojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPojoApplication.class, args);
+	}
+
+}
